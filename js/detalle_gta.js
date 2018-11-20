b4ad00071059cc59f1c0e7df210c5c24c96adbf6
xHernanL <44981427+xHernanL@users.noreply.github.com>
2018-11-19 23:16:47 -0300

    Refresh

--- js/detalle_gta.js
+++ js/detalle_gta.js
@@ -1,46 +1,46 @@
 document.addEventListener("DOMContentLoaded", function(){
-    juegos = JSON.parse(juegos);
+  juegos = JSON.parse(juegos);
 
-    var nombre = document.getElementById("nombre-gta");
-    nombre.innerHTML = juegos[0].nombre;
+  var nombre = document.getElementById("nombre-gta");
+  nombre.innerHTML = juegos[0].nombre;
 
-    var galeriaTitle = document.getElementById("galeria-title");
-    galeriaTitle.innerHTML = juegos[0].galeriatitle;
+  var galeriaTitle = document.getElementById("galeria-title");
+  galeriaTitle.innerHTML = juegos[0].galeriatitle;
 
-    var genero = document.getElementById("genero-gta");
-    genero.innerHTML = juegos[0].genero;
+  var genero = document.getElementById("genero-gta");
+  genero.innerHTML = juegos[0].genero;
 
-    var jugadores = document.getElementById("jugadores-gta");
-    jugadores.innerHTML = juegos[0].jugadores;
+  var jugadores = document.getElementById("jugadores-gta");
+  jugadores.innerHTML = juegos[0].jugadores;
 
-    var descripcion = document.getElementById("descripcion-gta");
-    descripcion.innerHTML = juegos[0].descripcion;
+  var descripcion = document.getElementById("descripcion-gta");
+  descripcion.innerHTML = juegos[0].descripcion;
 
-    var valoracion = document.getElementById("valoracion-gta");
-    valoracion.innerHTML = juegos[0].valoracion;
+  var valoracion = document.getElementById("valoracion-gta");
+  valoracion.innerHTML = juegos[0].valoracion;
 
-    var precio = document.getElementById("precio-gta");
-    precio.innerHTML = juegos[0].precio;
+  var precio = document.getElementById("precio-gta");
+  precio.innerHTML = juegos[0].precio;
 
-    var imgPortada = document.getElementById("img-portada");
-    imgPortada.setAttribute("src", juegos[0].portada);
+  var imgPortada = document.getElementById("img-portada");
+  imgPortada.setAttribute("src", juegos[0].portada);
 
-    var imgGaleria = document.getElementById("img-1");
-    imgGaleria.setAttribute("src", juegos[0].imagen1);
+  var imgGaleria = document.getElementById("img-1");
+  imgGaleria.setAttribute("src", juegos[0].imagen1);
 
-    var imgGaleria = document.getElementById("img-2");
-    imgGaleria.setAttribute("src", juegos[0].imagen2);
+  var imgGaleria = document.getElementById("img-2");
+  imgGaleria.setAttribute("src", juegos[0].imagen2);
 
-    var imgGaleria = document.getElementById("img-3");
-    imgGaleria.setAttribute("src", juegos[0].imagen3);
+  var imgGaleria = document.getElementById("img-3");
+  imgGaleria.setAttribute("src", juegos[0].imagen3);
 
-    var imgGaleria = document.getElementById("img-4");
-    imgGaleria.setAttribute("src", juegos[0].imagen4);
+  var imgGaleria = document.getElementById("img-4");
+  imgGaleria.setAttribute("src", juegos[0].imagen4);
 
-    var imgGaleria = document.getElementById("img-5");
-    imgGaleria.setAttribute("src", juegos[0].imagen5);
+  var imgGaleria = document.getElementById("img-5");
+  imgGaleria.setAttribute("src", juegos[0].imagen5);
 
-    var imgGaleria = document.getElementById("img-6");
-    imgGaleria.setAttribute("src", juegos[0].imagen6);
+  var imgGaleria = document.getElementById("img-6");
+  imgGaleria.setAttribute("src", juegos[0].imagen6);
 
-    });
+});
--- js/mi-script.js
+++ js/mi-script.js
@@ -1,64 +1,64 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    var precio = 40;
-    var costoEnvio = 0;
-    var subtotal = 0;
-    var num;
+  var precio = 40;
+  var costoEnvio = 0;
+  var subtotal = 0;
+  var num;
 
-    var campoCantidad = document.getElementById("cantidad");
-    var encabezadoSubtotal = document.getElementById("subtotal");
-    var encabezadoTotal = document.getElementById("total");
-    var envioComun = document.getElementById("comun");
-    var envioEspecial = document.getElementById("especial")
-    var envioPremium = document.getElementById("premium");
-    var totalEnvio = document.getElementById("total-envio");
-    var divOtorgado = document.getElementById("division-otorgado");
-    var radioDigital = document.getElementById("digital");
-    var radioFisico = document.getElementById("fisico");
+  var campoCantidad = document.getElementById("cantidad");
+  var encabezadoSubtotal = document.getElementById("subtotal");
+  var encabezadoTotal = document.getElementById("total");
+  var envioComun = document.getElementById("comun");
+  var envioEspecial = document.getElementById("especial")
+  var envioPremium = document.getElementById("premium");
+  var totalEnvio = document.getElementById("total-envio");
+  var divOtorgado = document.getElementById("division-otorgado");
+  var radioDigital = document.getElementById("digital");
+  var radioFisico = document.getElementById("fisico");
 
-    radioDigital.addEventListener("change", function(){
-        divOtorgado.style.display = "none";
+  radioDigital.addEventListener("change", function(){
+    divOtorgado.style.display = "none";
+  });
+
+  radioFisico.addEventListener("change", function(){
+    divOtorgado.style.display = "block";
+  });
+
+  campoCantidad.addEventListener("change", function() {
+    var cantidad = campoCantidad.value;
+    cantidad = parseInt(cantidad);
+
+    subtotal = cantidad * precio;
+    encabezadoSubtotal.innerHTML = subtotal;
+    num = subtotal * 1.22;
+    encabezadoTotal.innerHTML = num.toFixed(2);
+  });
+
+  envioComun.addEventListener("change", function () {
+    costoEnvio = subtotal * 0.005;
+    totalEnvio.innerHTML = costoEnvio;
+
+    encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
+    num = (costoEnvio + subtotal) * 1.22;
+    encabezadoTotal.innerHTML = num.toFixed(2);
+  });
+
+  envioEspecial.addEventListener("change", function () {
+    costoEnvio = subtotal * 0.02;
+    totalEnvio.innerHTML = costoEnvio;
+
+    encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
+    num = (costoEnvio + subtotal) * 1.22;
+    encabezadoTotal.innerHTML = num.toFixed(2);
     });
 
-    radioFisico.addEventListener("change", function(){
-        divOtorgado.style.display = "block";
-    });
 
-    campoCantidad.addEventListener("change", function() {
-      var cantidad = campoCantidad.value;
-      cantidad = parseInt(cantidad);
+  envioPremium.addEventListener("change", function () {
+    costoEnvio = subtotal * 0.05;
+    totalEnvio.innerHTML = costoEnvio;
 
-      subtotal = cantidad * precio;
-      encabezadoSubtotal.innerHTML = subtotal;
-      num = subtotal * 1.22;
-      encabezadoTotal.innerHTML = num.toFixed(2);
-    });
-
-    envioComun.addEventListener("change", function () {
-      costoEnvio = subtotal * 0.005;
-      totalEnvio.innerHTML = costoEnvio;
-
-      encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
-      num = (costoEnvio + subtotal) * 1.22;
-      encabezadoTotal.innerHTML = num.toFixed(2);
-    });
-
-    envioEspecial.addEventListener("change", function () {
-      costoEnvio = subtotal * 0.02;
-      totalEnvio.innerHTML = costoEnvio;
-
-      encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
-      num = (costoEnvio + subtotal) * 1.22;
-      encabezadoTotal.innerHTML = num.toFixed(2);
-    });
-
-
-    envioPremium.addEventListener("change", function () {
-      costoEnvio = subtotal * 0.05;
-      totalEnvio.innerHTML = costoEnvio;
-
-      encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
-      num = (costoEnvio + subtotal) * 1.22;
-      encabezadoTotal.innerHTML = num.toFixed(2);
+    encabezadoSubtotal.innerHTML = subtotal + costoEnvio;
+    num = (costoEnvio + subtotal) * 1.22;
+    encabezadoTotal.innerHTML = num.toFixed(2);
     });
 });
